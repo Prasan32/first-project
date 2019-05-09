4c582a2b144820e8df6e3037e49e6afd56d70a5f
Prasan32 <prasannakb440@gmail.com>
2019-05-09 13:25:44 +0545

    this is my first comit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,88 @@
+<nav class="navbar navbar-expand-md navbar-inverse navbar-laravel">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        <li class="nav-item active">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="/services" tabindex="-1" aria-disabled="true">Services</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link disabled" href="/posts" tabindex="-1" aria-disabled="true">Blog</a>
+      </li>
+      <!-- <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Others</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li> -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <!-- <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a> -->
+
+                             <!--    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                  <li><a href="/home">Dashboard</a></li>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div> -->
+                                 <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="/home" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dashboard</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+         <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <a><form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form></a>
+                                    <a href="#">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+        </div>
+      </li>
+                            </li>
+
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center ">
+        <h1>{{$title}}</h1>
+        <p>This is laravel video series from youtube laravel from scratch</p>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">login</a><a class="btn btn-success btn-lg" href="/
+        register" role="button">Register</a></p>
+    </div>
+@endsection
